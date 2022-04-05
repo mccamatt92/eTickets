0a57729a739085b2622fcbbf8be567741332104f
Matthias McCarthy <matthias.mccarthy@gmail.com>
2022-04-05 18:43:00 +0200

    Setup of Models and DbContext

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using eTickets.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+
+        public string MovieName { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public string ImageUrl { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+        public MovieCategory MovieCategory { get; set; }
+
+
+    }
+}
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,11 @@
+using eTickets.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
@@ -15,6 +19,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+
 
 app.UseRouting();
 
